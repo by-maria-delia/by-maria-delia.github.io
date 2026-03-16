d5394c984d6248d9be80b2ddb8a9536332a2d9d8
Lucas Avendano <lzarza@getsporttrade.com>
2026-03-15 23:18:03 -0300

    refactor: swap display font to Oooh Baby

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HowItWorks from './components/HowItWorks'
-import ModelsGallery from './components/ModelsGallery'
-import Showcase from './components/Showcase'
-import DeliveryInfo from './components/DeliveryInfo'
-import Footer from './components/Footer'
+import DeliveryInfo from "./components/DeliveryInfo";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import HowItWorks from "./components/HowItWorks";
+import ModelsGallery from "./components/ModelsGallery";
+import Navbar from "./components/Navbar";
+import Showcase from "./components/Showcase";
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-soft-white">
-      <Navbar />
-      <main>
-        <Hero />
-        <HowItWorks />
-        <ModelsGallery />
-        <Showcase />
-        <DeliveryInfo />
-      </main>
-      <Footer />
-    </div>
-  )
+	return (
+		<div className="min-h-screen bg-soft-white">
+			<Navbar />
+			<main>
+				<Hero />
+				<HowItWorks />
+				<ModelsGallery />
+				<Showcase />
+				<DeliveryInfo />
+			</main>
+			<Footer />
+		</div>
+	);
 }
